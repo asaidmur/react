dad0b91269678e484b975c91f35e01bc7be98368
Ardasher Sayid <adash>
2020-01-14 17:22:14 -0800

    Updates

---
+++ precache-manifest.55b62065f3ba21b8e90b95b4dc3f163e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86372748fbcbe7ea7645a6b23c607c30",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "3c0480ebfdfcf621d6e5",
+    "url": "/react/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3c0480ebfdfcf621d6e5",
+    "url": "/react/static/js/2.6942ee37.chunk.js"
+  },
+  {
+    "revision": "c8336e0bcbf8ea1670aafb15aa044bf7",
+    "url": "/react/static/js/2.6942ee37.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55152e53af96bb70f865",
+    "url": "/react/static/js/main.189f5ad6.chunk.js"
+  },
+  {
+    "revision": "7f968a846eff801b74c2",
+    "url": "/react/static/js/runtime-main.229e7660.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.589dd7a3b5a309de19649cd0a95e20f8.js"
+  "/react/precache-manifest.55b62065f3ba21b8e90b95b4dc3f163e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
